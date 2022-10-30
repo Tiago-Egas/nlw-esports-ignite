ffce978829abe1426eeeb33e861ed895de0b4f72
tiago-egas <tiago.egas@outlook.com>
2022-10-30 02:41:07 -0300

    Lecture 1 finished

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,10 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+
+function Button(props: ButtonProps) {
+  return (
+    <TouchableOpacity>
+      <Text>{props.title}</Text>
+    </TouchableOpacity>
+  );
+}
+interface ButtonProps {
+  title: string;
+}
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>React Native!</Text>
+      <Text style={styles.title}>React Native!</Text>
+
+      <Button title='Hello, Wolrd!' />
+
       <StatusBar style='auto' />
     </View>
   );
@@ -16,5 +30,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  title: {
+    color: '#000',
+    fontSize: 32,
   },
 });
